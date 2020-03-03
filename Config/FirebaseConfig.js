ba3173ced724a74cb0558ea835b472b8e0b960c6
Shuvo <shovocse753@gmail.com>
2020-03-03 22:46:08 +0600

    FirebaseConfig.js is created and global variable firebaseConfig is exported and imported in school.insert.js

---
+++ Config/FirebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDlESaTQUPM3rRX_BBLLihLfVf3UEEfBmc",
+    authDomain: "admission-consultant-c9db1.firebaseapp.com",
+    databaseURL: "https://admission-consultant-c9db1.firebaseio.com",
+    projectId: "admission-consultant-c9db1",
+    storageBucket: "admission-consultant-c9db1.appspot.com",
+    messagingSenderId: "1006379150064",
+    appId: "1:1006379150064:web:2d4cb80711383179ef7035",
+    measurementId: "G-P82D3N1F4K"
+  };
+
+  export {firebaseConfig};
